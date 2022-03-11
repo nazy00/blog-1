c43b50ec04e59981e056fdf440103e088986fac0
Naz <naziya@cybar.dev>
2022-03-11 18:00:26 -0400

    use Alpine.js

--- script.js
+++ script.js
@@ -1,27 +1,75 @@
 // @ts-nocheck
-const DEFAULT_H1 = "Word Writer";
-const CAPS_H1 = "CAPS ON";
-let isCaps = false;
+const $ = (id) => document.querySelector(id);
+const $_ = (id) => document.querySelectorAll(id);
 
-function addChar(selection) {
-  var currChars = document.getElementById("words").value;
-  if (selection === "bksp") {
-    document.getElementById("words").value = currChars.substring(0, currChars.length - 1);
-  } else {
-    if (isCaps) {
-      selection = selection.toUpperCase();
-      isCaps = false;
-      document.getElementsByTagName("h1").innerHTML = DEFAULT_H1;
+const staticData = {
+  kbdFocus: null,
+  shiftOn: false,
+
+  addChar(selection) {
+    let words = this.kbdFocus ? this.kbdFocus : $("#editbox");
+    let currChars = words.value;
+    if (selection === "del") {
+      words.value = currChars.substring(
+        0,
+        currChars.length - 1
+      );
+    } else {
+      if (this.shiftOn) {
+        selection = selection.toUpperCase();
+        shiftOn = !shiftOn;
+      }
+      words.value = currChars.concat(selection);
     }
-    document.getElementById("words").value = currChars.concat(selection);
-  }
-}
-function enter() {
-  var content = document.getElementById("words").value;
-  console.log(content);
-  document.getElementById("words").value = "";
-}
-function caps() {
-  isCaps = true;
-  document.getElementsByTagName("h1").innerHTML = CAPS_H1;
-}
+  },
+
+  sp: {
+    shift: "shift",
+    delete: "delete",
+    return: "return",
+    space: "space",
+  },
+
+  glyphs: [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "q",
+    "w",
+    "e",
+    "r",
+    "t",
+    "y",
+    "u",
+    "i",
+    "o",
+    "p",
+    "a",
+    "s",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    '"',
+    "z",
+    "x",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    ".",
+    "?",
+    ",",
+  ],
+};
